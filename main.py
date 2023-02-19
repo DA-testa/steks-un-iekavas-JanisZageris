2d5cf66aaeb4017c6b6906f00f6069546759a495
JanisZageris <125265062+JanisZageris@users.noreply.github.com>
2023-02-19 13:10:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# python3
+# 221RDB249 Jānis Kārlis Zāģeris 2. grupa
 
 from collections import namedtuple
 
@@ -11,20 +11,28 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+    for i, next_char in enumerate(text):
+        if next_char in "([{":                                        # Pārbauda atverošās iekavas
+            opening_brackets_stack.append(Bracket(next_char, i))
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+        elif next_char in ")]}":                                      # Pārbauda aizverošās iekavas
+            if not opening_brackets_stack:                            # Ja nav atverošās iekavas
+                return i+1
+            else:
+                top = opening_brackets_stack.pop()
+                if not are_matching(top.char, next_char):             # Ja atverošā iekava nesakrīt
+                    return i+1
+
+    if opening_brackets_stack:                                        # Ja joprojām ir palikušas atverošās iekavas un nav tām attiecīgās aizverošās iekavas
+        return opening_brackets_stack[0].position+1
+
+    return "Success"                                                  # Ja viss sakrīt
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
